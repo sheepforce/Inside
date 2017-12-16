8ef1ef1284950b1bd8bfa178d7bcd02ba0c6be68
Phillip Seeber <phillip@manaan.cluster.local>
2017-12-16 02:40:05 +0100

    Generation of CICommands using lenses

--- src/Hardware/Vacom/Coldion.hs
+++ src/Hardware/Vacom/Coldion.hs
@@ -1,76 +1,63 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Hardware.Vacom.Coldion
 ( CICommand
 , CIString
-, ciString2Word8s
 , ciString2ByteString
+, createCommandCIString
 ) where
-import qualified Data.ByteString               as B
-import qualified Data.ByteString.Builder       as B
-import qualified Data.ByteString.Char8         as C
-import qualified Data.ByteString.Conversion.To as B
+import           Control.Lens
+import qualified Data.ByteString         as B
+--import qualified Data.ByteString.Builder       as B
+--import qualified Data.ByteString.Char8         as C
+--import qualified Data.ByteString.Conversion.To as B
 import           Data.Maybe
 import           Data.Word
 import           Internal.BinaryMessages
 
--- commands known to the Coldion AND this programm
+-- | commands known to the Coldion AND this programm
 data CICommand =
-  AskPressure Int
+  AskPressure Word8
   deriving (Show, Eq)
 
--- structure of communication strings from and to the device
+isAskPressure :: CICommand -> Bool
+isAskPressure (AskPressure _ ) = True
+isAskPressure _                = False
+
+fromAskPressure :: CICommand -> Maybe Word8
+fromAskPressure (AskPressure a) = Just a
+fromAskPressure _               = Nothing
+
+-- | structure of communication strings from and to the device
 data CIString = CIString
-  { ci_start          :: Word8          -- the start byte (A5)
-  , ci_header         :: Word8          -- the header byte
-  , ci_adressReceiver :: Word8          -- adress of the receiver (00 for RS232)
-  , ci_adressSender   :: Word8          -- adress of the sender (00 for RS232)
-  , ci_command        :: (Word8, Word8) -- command and subcommand
-  , ci_data           :: ( Word8        -- the data bytes, 16 of them
-                         , Word8
-                         , Word8
-                         , Word8
-                         , Word8
-                         , Word8
-                         , Word8
-                         , Word8
-                         , Word8
-                         , Word8
-                         , Word8
-                         , Word8
-                         , Word8
-                         , Word8
-                         , Word8
-                         , Word8 )
-  , ci_checksum       :: (Word8, Word8) -- the checksum, two bytes
+  { _ci_start          :: Word8          -- the start byte (A5)
+  , _ci_header         :: Word8          -- the header byte
+  , _ci_adressReceiver :: Word8          -- adress of the receiver (00 for RS232)
+  , _ci_adressSender   :: Word8          -- adress of the sender (00 for RS232)
+  , _ci_command        :: (Word8, Word8) -- command and subcommand
+  , _ci_data           :: ( Word8        -- the data bytes, 16 of them
+                          , Word8
+                          , Word8
+                          , Word8
+                          , Word8
+                          , Word8
+                          , Word8
+                          , Word8
+                          , Word8
+                          , Word8
+                          , Word8
+                          , Word8
+                          , Word8
+                          , Word8
+                          , Word8
+                          , Word8 )
+  , _ci_checksum       :: (Word8, Word8) -- the checksum, two bytes
   }
+makeLenses ''CIString
 
 instance Show CIString where
-  show (CIString a b c d e f g) =
-    word8String a ++ " " ++
-    word8String b ++ " " ++
-    word8String c ++ " " ++
-    word8String d ++ " " ++
-    (word8String . fst $ e) ++ " " ++
-    (word8String . snd $ e) ++ " " ++
-    (word8String . (\(x, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) -> x) $ f) ++ " " ++
-    (word8String . (\(_, x, _, _, _, _, _, _, _, _, _, _, _, _, _, _) -> x) $ f) ++ " " ++
-    (word8String . (\(_, _, x, _, _, _, _, _, _, _, _, _, _, _, _, _) -> x) $ f) ++ " " ++
-    (word8String . (\(_, _, _, x, _, _, _, _, _, _, _, _, _, _, _, _) -> x) $ f) ++ " " ++
-    (word8String . (\(_, _, _, _, x, _, _, _, _, _, _, _, _, _, _, _) -> x) $ f) ++ " " ++
-    (word8String . (\(_, _, _, _, _, x, _, _, _, _, _, _, _, _, _, _) -> x) $ f) ++ " " ++
-    (word8String . (\(_, _, _, _, _, _, x, _, _, _, _, _, _, _, _, _) -> x) $ f) ++ " " ++
-    (word8String . (\(_, _, _, _, _, _, _, x, _, _, _, _, _, _, _, _) -> x) $ f) ++ " " ++
-    (word8String . (\(_, _, _, _, _, _, _, _, x, _, _, _, _, _, _, _) -> x) $ f) ++ " " ++
-    (word8String . (\(_, _, _, _, _, _, _, _, _, x, _, _, _, _, _, _) -> x) $ f) ++ " " ++
-    (word8String . (\(_, _, _, _, _, _, _, _, _, _, x, _, _, _, _, _) -> x) $ f) ++ " " ++
-    (word8String . (\(_, _, _, _, _, _, _, _, _, _, _, x, _, _, _, _) -> x) $ f) ++ " " ++
-    (word8String . (\(_, _, _, _, _, _, _, _, _, _, _, _, x, _, _, _) -> x) $ f) ++ " " ++
-    (word8String . (\(_, _, _, _, _, _, _, _, _, _, _, _, _, x, _, _) -> x) $ f) ++ " " ++
-    (word8String . (\(_, _, _, _, _, _, _, _, _, _, _, _, _, _, x, _) -> x) $ f) ++ " " ++
-    (word8String . (\(_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, x) -> x) $ f) ++ " " ++
-    (word8String . fst $ g) ++ " " ++
-    (word8String . snd $ g)
+  show a = concat . map ((++ " ") . word8String). ciString2Word8s $ a
 
--- convert a CIString to a list of Word8 (24 Byte Sequence)
+-- | convert a CIString to a list of Word8 (24 Byte Sequence)
 ciString2Word8s :: CIString -> [Word8]
 ciString2Word8s input =
   [ b0 , b1 , b2 , b3
@@ -80,89 +67,89 @@ ciString2Word8s input =
   , b16, b17, b18, b19
   , b20, b21, b22, b23 ]
   where
-    b0  = ci_start input
-    b1  = ci_header input
-    b2  = ci_adressReceiver input
-    b3  = ci_adressSender input
-    b4  = fst . ci_command $ input
-    b5  = snd . ci_command $ input
-    b6  = (\(a, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) -> a) . ci_data $ input
-    b7  = (\(a, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) -> a) . ci_data $ input
-    b8  = (\(a, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) -> a) . ci_data $ input
-    b9  = (\(a, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) -> a) . ci_data $ input
-    b10 = (\(a, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) -> a) . ci_data $ input
-    b11 = (\(a, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) -> a) . ci_data $ input
-    b12 = (\(a, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) -> a) . ci_data $ input
-    b13 = (\(a, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) -> a) . ci_data $ input
-    b14 = (\(a, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) -> a) . ci_data $ input
-    b15 = (\(a, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) -> a) . ci_data $ input
-    b16 = (\(a, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) -> a) . ci_data $ input
-    b17 = (\(a, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) -> a) . ci_data $ input
-    b18 = (\(a, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) -> a) . ci_data $ input
-    b19 = (\(a, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) -> a) . ci_data $ input
-    b20 = (\(a, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) -> a) . ci_data $ input
-    b21 = (\(a, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) -> a) . ci_data $ input
-    b22 = fst . ci_checksum $ input
-    b23 = snd . ci_checksum $ input
+    b0  = _ci_start input
+    b1  = _ci_header input
+    b2  = _ci_adressReceiver input
+    b3  = _ci_adressSender input
+    b4  = fst . _ci_command $ input
+    b5  = snd . _ci_command $ input
+    b6  = (\(a, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) -> a) . _ci_data $ input
+    b7  = (\(_, a, _, _, _, _, _, _, _, _, _, _, _, _, _, _) -> a) . _ci_data $ input
+    b8  = (\(_, _, a, _, _, _, _, _, _, _, _, _, _, _, _, _) -> a) . _ci_data $ input
+    b9  = (\(_, _, _, a, _, _, _, _, _, _, _, _, _, _, _, _) -> a) . _ci_data $ input
+    b10 = (\(_, _, _, _, a, _, _, _, _, _, _, _, _, _, _, _) -> a) . _ci_data $ input
+    b11 = (\(_, _, _, _, _, a, _, _, _, _, _, _, _, _, _, _) -> a) . _ci_data $ input
+    b12 = (\(_, _, _, _, _, _, a, _, _, _, _, _, _, _, _, _) -> a) . _ci_data $ input
+    b13 = (\(_, _, _, _, _, _, _, a, _, _, _, _, _, _, _, _) -> a) . _ci_data $ input
+    b14 = (\(_, _, _, _, _, _, _, _, a, _, _, _, _, _, _, _) -> a) . _ci_data $ input
+    b15 = (\(_, _, _, _, _, _, _, _, _, a, _, _, _, _, _, _) -> a) . _ci_data $ input
+    b16 = (\(_, _, _, _, _, _, _, _, _, _, a, _, _, _, _, _) -> a) . _ci_data $ input
+    b17 = (\(_, _, _, _, _, _, _, _, _, _, _, a, _, _, _, _) -> a) . _ci_data $ input
+    b18 = (\(_, _, _, _, _, _, _, _, _, _, _, _, a, _, _, _) -> a) . _ci_data $ input
+    b19 = (\(_, _, _, _, _, _, _, _, _, _, _, _, _, a, _, _) -> a) . _ci_data $ input
+    b20 = (\(_, _, _, _, _, _, _, _, _, _, _, _, _, _, a, _) -> a) . _ci_data $ input
+    b21 = (\(_, _, _, _, _, _, _, _, _, _, _, _, _, _, _, a) -> a) . _ci_data $ input
+    b22 = fst . _ci_checksum $ input
+    b23 = snd . _ci_checksum $ input
 
--- convert a CIString to a non-lazy ByteString (8 bit sequence)
+-- | convert a CIString to a non-lazy ByteString (8 bit sequence)
 ciString2ByteString :: CIString -> B.ByteString
 ciString2ByteString = B.pack . ciString2Word8s
 
--- create a CIString for given Commands
+-- | the default CIString, that will be updated
+defCIString :: CIString
+defCIString =
+  CIString
+    { _ci_start          = 0xA5
+    , _ci_header         = 0x50
+    , _ci_adressReceiver = 0x00
+    , _ci_adressSender   = 0x00
+    , _ci_command        = (0x00, 0x00)
+    , _ci_data           = ( 0x00
+                           , 0x00
+                           , 0x00
+                           , 0x00
+                           , 0x00
+                           , 0x00
+                           , 0x00
+                           , 0x00
+                           , 0x00
+                           , 0x00
+                           , 0x00
+                           , 0x00
+                           , 0x00
+                           , 0x00
+                           , 0x00
+                           , 0x00 )
+    , _ci_checksum       = (0x00, 0x00)
+    }
+
+-- | starting from the default CIString update the CIString based on the CICommand
+-- | that is supplied.
+-- | updates of the CIString happen using Control.Lens operators
 createCommandCIString :: CICommand -> Maybe CIString
 createCommandCIString a
-  | a == AskPressure 1 =
-    Just CIString { ci_start          = startB
-                  , ci_header         = headerB
-                  , ci_adressReceiver = adressReceiverB
-                  , ci_adressSender   = adressSenderB
-                  , ci_command        = (0x20, 0x10)
-                  , ci_data           = ( 0x01
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00 )
-                  , ci_checksum       = (0xFF, 0x8C)
-                  }
-  | a == AskPressure 2 =
-    Just CIString { ci_start          = startB
-                  , ci_header         = headerB
-                  , ci_adressReceiver = adressReceiverB
-                  , ci_adressSender   = adressSenderB
-                  , ci_command        = (0x20, 0x10)
-                  , ci_data           = ( 0x02
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00
-                                        , 0x00 )
-                  , ci_checksum       = (0xBF, 0x7D)
-                  }
-  | otherwise          = Nothing
+  | isAskPressure a && fromJust pressureChannel == 0x01 =
+    Just
+    $ defCIString
+    & ci_command .~ (0x20, 0x10)
+    & ci_data .~ ( fromJust $ pressureChannel
+                 , 0x00
+                 , 0x00
+                 , 0x00
+                 , 0x00
+                 , 0x00
+                 , 0x00
+                 , 0x00
+                 , 0x00
+                 , 0x00
+                 , 0x00
+                 , 0x00
+                 , 0x00
+                 , 0x00
+                 , 0x00
+                 , 0x00 )
+    & ci_checksum .~ (0xFF, 0x8C)
+  | otherwise = Nothing
   where
-    startB = 0xA5
-    headerB = 0x50
-    adressReceiverB = 0x00
-    adressSenderB = 0x00
+    pressureChannel = fromAskPressure a
