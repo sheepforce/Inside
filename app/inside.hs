9006617f5779ca62ada30a3844f720b7d8a47e75
Phillip Seeber <phillip@fkpc55-debian.ifk.uni-jena.de>
2017-12-18 17:35:28 +0100

    expanding tui with brick

--- app/inside.hs
+++ app/inside.hs
@@ -2,12 +2,16 @@
 {-# LANGUAGE TemplateHaskell   #-}
 
 import           Brick
-import           Brick.BChan            (newBChan, writeBChan)
-import           Control.Concurrent     (forkIO, threadDelay)
-import           Control.Monad          (forever, void)
+import           Brick.BChan                (newBChan, writeBChan)
+import qualified Brick.Widgets.Border       as BB
+import qualified Brick.Widgets.Border.Style as BBS
+import qualified Brick.Widgets.Center       as BC
+import           Control.Concurrent         (forkIO, threadDelay)
+import           Control.Monad              (forever, void)
+import           Data.Maybe
 import           Data.Word
-import qualified Graphics.Vty           as V
-import qualified Hardware.Vacom.Coldion as CI
+import qualified Graphics.Vty               as V
+import qualified Hardware.Vacom.Coldion     as CI
 import           Lens.Micro
 import           Lens.Micro.TH
 
@@ -24,13 +28,26 @@ data Tick = Tick
 -- | a type, which is empty, so that it is easy to change things later.
 type Name = ()
 
+-- | The ColdIon interface. What it should do and on which port (something like
+-- | /dev/ttyUSB0) the device is connected
+type Pressure = Double
+data ColdIon = ColdIon
+  { _task     :: CI.CICommand
+  , _port     :: FilePath
+  , _pressure :: Maybe Pressure
+  , _devName  :: Maybe String
+  } deriving (Show)
+makeLenses ''ColdIon
+
 -- | The user interface mainly takes care of displaying things. Therefore
 -- | the state of our app is only described by tasks for specific devices, that
 -- | shall be carried out in the current loop
 data Measurements = Measurements
-  { _coldionTask :: CI.CICommand -- what is the Coldion CU-100 supposed to do?
+  { _coldIon :: ColdIon -- what is the Coldion CU-100 supposed to do?
   } deriving (Show)
 makeLenses ''Measurements
+
+
 
 
 {- ########################################################################## -}
@@ -38,7 +55,7 @@ makeLenses ''Measurements
 {- ########################################################################## -}
 -- | defining the Brick app type
 app = App
-  { appDraw         = coldionWidget           -- these are the visual elements
+  { appDraw         = coldionWidget                  -- these are the visual elements
                                               -- shown (aka layout), as well as
                                               -- the functionality the have
 
@@ -60,7 +77,18 @@ app = App
 -- | widget for holding the Coldion pressure
 coldionWidget :: Measurements -> [Widget Name]
 coldionWidget m =
-  [ withAttr "pressureAttr" $ str $ show (_coldionTask m) ]
+  [
+  hLimit 15
+  $ withBorderStyle BBS.unicodeBold
+  $ BB.borderWithLabel (str devName)
+  $ BC.hCenter
+  $ padAll 1
+  $ str $ show $ fromMaybe 0.0 (m ^. (coldIon . pressure))
+  ]
+  where
+    devName = "ColdIon CU-100"
+    -- pressure = (m ^. (coldIon . pressure)) :: Double
+
 
 -- | handling tick event
 handleEvent :: Measurements -> BrickEvent Name Tick -> EventM Name (Next Measurements)
@@ -70,6 +98,18 @@ handleEvent m (VtyEvent (V.EvKey V.KEsc [])) = halt m
 theMeasurements :: AttrMap
 theMeasurements = attrMap V.defAttr [("pressureAttr" :: AttrName, V.red `on` V.blue)]
 
+
+initColdIon = ColdIon
+  { _task = CI.GetDevName
+  , _port = "/dev/ttyUSB0"
+  , _pressure = Nothing
+  , _devName = Nothing
+  }
+
+initMeasurement = Measurements
+ { _coldIon = initColdIon
+ }
+
 main :: IO ()
 main = do
   chan <- newBChan 10
@@ -77,6 +117,6 @@ main = do
     writeBChan chan Tick
     threadDelay 100000
 
-  let initMeasurement = Measurements {_coldionTask = CI.AskPressure 0x01}
+
 
   void $ customMain (V.mkVty V.defaultConfig) (Just chan) app initMeasurement
