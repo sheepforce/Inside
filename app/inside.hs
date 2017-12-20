2919f2a682f8b9dfd37f98e43717276a9fe988f9
Phillip Seeber <phillip@fkpc55-debian.ifk.uni-jena.de>
2017-12-20 17:05:09 +0100

    adding two GraphixThrees to the TUI

--- app/inside.hs
+++ app/inside.hs
@@ -20,6 +20,7 @@ import qualified Data.Text.IO                             as T
 import           Data.Word
 import qualified Graphics.Vty                             as V
 import qualified Hardware.LakeShore.TemperatureController as LS
+import qualified Hardware.Leybold.GraphixThree            as GT
 import qualified Hardware.Vacom.Coldion                   as CI
 import           Lens.Micro
 import           Lens.Micro.TH
@@ -63,12 +64,36 @@ data LakeShore = LakeShore
   } deriving (Show)
 makeLenses ''LakeShore
 
+-- | The firtst of the to GraphixThree1 controllers, 3 gauges
+type GT1Pressures = (Maybe Double, Maybe Double, Maybe Double)
+data GraphixThree1 = GraphixThree1
+  { _gt1Enabled   :: Bool
+  , _gt1Task      :: GT.GTCommand
+  , _gt1Port      :: FilePath
+  , _gt1Pressures :: GT1Pressures
+  , _gt1DevName   :: Maybe String
+  } deriving (Show)
+makeLenses '' GraphixThree1
+
+-- | The second of the to GraphixThree1 controllers, 3 gauges
+type GT2Pressures = (Maybe Double, Maybe Double, Maybe Double)
+data GraphixThree2 = GraphixThree2
+  { _gt2Enabled   :: Bool
+  , _gt2Task      :: GT.GTCommand
+  , _gt2Port      :: FilePath
+  , _gt2Pressures :: GT2Pressures
+  , _gt2DevName   :: Maybe String
+  } deriving (Show)
+makeLenses '' GraphixThree2
+
 -- | The user interface mainly takes care of displaying things. Therefore
 -- | the state of our app is only described by tasks for specific devices, that
 -- | shall be carried out in the current loop
 data Measurements = Measurements
-  { _coldIon   :: ColdIon     -- infos about the Coldion CU-100
-  , _lakeShore :: LakeShore -- infos about the LakeShore 335
+  { _coldIon       :: ColdIon       -- infos about the Coldion CU-100
+  , _lakeShore     :: LakeShore     -- infos about the LakeShore 335
+  , _graphixThree1 :: GraphixThree1 -- infos about the first GraphixThree
+  , _graphixThree2 :: GraphixThree2 -- infos about the second GraphixThree
   } deriving (Show)
 makeLenses ''Measurements
 
@@ -101,12 +126,12 @@ app = App
 -- | the user interface, composed of individual Widgets
 drawUI :: Measurements -> [Widget Name]
 drawUI m =
-  [ BC.center  -- $(coldionWidgetPressure m) <=> (deviceWidgetColdIon m)]
-  $ hBox [coldionWidgetPressure m, lakeShoreWidgetTemperature m]
+  [ BC.center
+  $ hBox [coldionWidgetPressure m, lakeShoreWidgetTemperature m, graphixThree1WidgetPressure m, graphixThree2WidgetPressure m]
   <=>
-  hBox [deviceWidgetColdIon m, deviceWidgetLakeShore m]
+  hBox [deviceWidgetColdIon m, deviceWidgetLakeShore m, deviceWidgetGraphixThree1 m, deviceWidgetGraphixThree2 m]
   <=>
-  hBox [coldIonWarningWidget m, lakeShoreWarningWidget m]
+  hBox [coldIonWarningWidget m, lakeShoreWarningWidget m, graphixThree1WarningWidget m, graphixThree2WarningWidget m]
   ]
 
 {- ======= -}
@@ -209,6 +234,113 @@ lakeShoreWarningWidget m =
       then str "WARNING"
       else str "  OK   "
 
+{- ============== -}
+{- GraphixThree 1 -}
+{- ============== -}
+graphixThree1WidgetPressure :: Measurements -> Widget Name
+graphixThree1WidgetPressure m =
+  hLimit 20
+  $ withBorderStyle BBS.unicodeBold
+  $ BB.borderWithLabel (str shownDevName)
+  $ BC.hCenter
+  $ padAll 1
+  $ vBox
+  [ str $ show $ fromMaybe 0.0 pressureA
+  , str $ show $ fromMaybe 0.0 pressureB
+  , str $ show $ fromMaybe 0.0 pressureC
+  ]
+  where
+    shownDevName = fromMaybe "GraphixThree (1)" $ m ^. (graphixThree1 . gt1DevName)
+    maybePressures = m ^. (graphixThree1 . gt1Pressures)
+    pressureA = maybePressures ^. _1
+    pressureB = maybePressures ^. _2
+    pressureC = maybePressures ^. _3
+
+deviceWidgetGraphixThree1 :: Measurements -> Widget Name
+deviceWidgetGraphixThree1 m =
+  hLimit 20
+  $ withBorderStyle BBS.unicodeBold
+  $ BB.borderWithLabel (str "GraphixThree (1)")
+  $ padAll 1
+  $ str
+  $  ""
+  ++ "  Enabled:\n"
+  ++ "    " ++ shownEnabled ++ "\n\n"
+  ++ "  Port:\n"
+  ++ "    " ++ shownPort ++ "\n\n"
+  ++ "  Device Name:\n"
+  ++ "    " ++ show shownDevName
+  where
+    shownEnabled = show $ m ^. (graphixThree1 . gt1Enabled)
+    shownPort = m ^. (graphixThree1 . gt1Port)
+    shownDevName = m ^. (graphixThree1 . gt1DevName)
+
+graphixThree1WarningWidget :: Measurements -> Widget Name
+graphixThree1WarningWidget m =
+  hLimit 20
+  $ withBorderStyle BBS.unicodeBold
+  $ BB.borderWithLabel (str "LakeShore 335")
+  $ padAll 1
+  $ if (  fromMaybe 0.0 (m ^. graphixThree1 . gt1Pressures . _1) >= 300.0
+       || fromMaybe 0.0 (m ^. graphixThree1 . gt1Pressures . _2) >= 300.0
+       || fromMaybe 0.0 (m ^. graphixThree1 . gt1Pressures . _3) >= 300.0 )
+      then str "WARNING"
+      else str "  OK   "
+
+{- ============== -}
+{- GraphixThree 2 -}
+{- ============== -}
+graphixThree2WidgetPressure :: Measurements -> Widget Name
+graphixThree2WidgetPressure m =
+  hLimit 20
+  $ withBorderStyle BBS.unicodeBold
+  $ BB.borderWithLabel (str shownDevName)
+  $ BC.hCenter
+  $ padAll 1
+  $ vBox
+  [ str $ show $ fromMaybe 0.0 pressureA
+  , str $ show $ fromMaybe 0.0 pressureB
+  , str $ show $ fromMaybe 0.0 pressureC
+  ]
+  where
+    shownDevName = fromMaybe "GraphixThree (2)" $ m ^. (graphixThree2 . gt2DevName)
+    maybePressures = m ^. (graphixThree2 . gt2Pressures)
+    pressureA = maybePressures ^. _1
+    pressureB = maybePressures ^. _2
+    pressureC = maybePressures ^. _3
+
+deviceWidgetGraphixThree2 :: Measurements -> Widget Name
+deviceWidgetGraphixThree2 m =
+  hLimit 20
+  $ withBorderStyle BBS.unicodeBold
+  $ BB.borderWithLabel (str "GraphixThree (2)")
+  $ padAll 1
+  $ str
+  $  ""
+  ++ "  Enabled:\n"
+  ++ "    " ++ shownEnabled ++ "\n\n"
+  ++ "  Port:\n"
+  ++ "    " ++ shownPort ++ "\n\n"
+  ++ "  Device Name:\n"
+  ++ "    " ++ show shownDevName
+  where
+    shownEnabled = show $ m ^. (graphixThree2 . gt2Enabled)
+    shownPort = m ^. (graphixThree2 . gt2Port)
+    shownDevName = m ^. (graphixThree2 . gt2DevName)
+
+graphixThree2WarningWidget :: Measurements -> Widget Name
+graphixThree2WarningWidget m =
+  hLimit 20
+  $ withBorderStyle BBS.unicodeBold
+  $ BB.borderWithLabel (str "LakeShore 335")
+  $ padAll 1
+  $ if (  fromMaybe 0.0 (m ^. graphixThree2 . gt2Pressures . _1) >= 300.0
+       || fromMaybe 0.0 (m ^. graphixThree2 . gt2Pressures . _2) >= 300.0
+       || fromMaybe 0.0 (m ^. graphixThree2 . gt2Pressures . _3) >= 300.0 )
+      then str "WARNING"
+      else str "  OK   "
+
+
 {- ########################################################################## -}
 {- Define EventHandling -}
 {- ########################################################################## -}
@@ -218,6 +350,8 @@ handleEvent :: Measurements -> BrickEvent Name Tick -> EventM Name (Next Measure
 handleEvent m (AppEvent Tick)                       = liftIO (getCurrentConditions m) >>= continue
 handleEvent m (VtyEvent (V.EvKey (V.KChar 'c') [])) = continue $ toggleColdIon m
 handleEvent m (VtyEvent (V.EvKey (V.KChar 'l') [])) = continue $ toggleLakeShore m
+handleEvent m (VtyEvent (V.EvKey (V.KChar 'g') [])) = continue $ toggleGraphixThree1 m
+handleEvent m (VtyEvent (V.EvKey (V.KChar 't') [])) = continue $ toggleGraphixThree2 m
 handleEvent m (VtyEvent (V.EvKey V.KEsc []))        = halt m
 handleEvent m _                                     = continue m
 
@@ -234,15 +368,25 @@ getCurrentConditions m = do
     if (m ^. lakeShore . lsEnabled)          -- if enabled
       then lakeShoreTempUpdate m
       else return (Nothing, Nothing)
+  newGraphixThree1Pressures <-
+    if (m ^. graphixThree1 . gt1Enabled)
+      then graphixThree1PressureUpdate m
+      else return (Nothing, Nothing, Nothing)
+  newGraphixThree2Pressures <-
+    if (m ^. graphixThree2 . gt2Enabled)
+      then graphixThree2PressureUpdate m
+      else return (Nothing, Nothing, Nothing)
   return $
     m
-    & coldIon . ciPressure .~ newColdIonPressure             -- write pressure of ColdIon
-    & lakeShore . lsTemperatures .~ newLakeShoreTemperaturss -- write Temperatures of LS
+    & coldIon . ciPressure .~ newColdIonPressure                -- write pressure of ColdIon
+    & lakeShore . lsTemperatures .~ newLakeShoreTemperaturss    -- write Temperatures of LS
+    & graphixThree1 . gt1Pressures .~ newGraphixThree1Pressures -- write Pressures of GT1
+    & graphixThree2 . gt2Pressures .~ newGraphixThree2Pressures -- write Pressures of GT2
 
 {- ======= -}
 {- ColdIon -}
 {- ======= -}
--- | Sending, receiving and in die gasphase 😀 und die meisten gruppen, kenn ich ja schon, hab ich ja reingeschmissenunderstanding an request to the ColdIon.
+-- | Sending, receiving and understanding an request to the ColdIon.
 -- | The pressure is retured
 coldIonPressureUpdate :: Measurements -> IO (Maybe Double)
 coldIonPressureUpdate m = do
@@ -287,15 +431,15 @@ coldIonNameUpdate m = do
   let coldIonAnswerCIString = AB.parseOnly CI.parseAnswer coldIonAnswer
   if isRight coldIonAnswerCIString
     then do
-      let ciDevName =
+      let coldIonDevName =
             AB.parseOnly CI.parseName
             $ B.pack
             . CI.dataBytes2List
             . CI._ci_data
             $ fromRight coldIonAnswerCIString
 
-      if isRight ciDevName
-        then return $ Just (fromRight ciDevName)
+      if isRight coldIonDevName
+        then return $ Just (fromRight coldIonDevName)
         else return Nothing
     else return Nothing
   where
@@ -345,6 +489,130 @@ toggleLakeShore :: Measurements -> Measurements
 toggleLakeShore m = m & lakeShore . lsEnabled .~ not currVal
   where
     currVal = m ^. lakeShore . lsEnabled
+
+{- ============== -}
+{- GraphixThree 1 -}
+{- ============== -}
+-- | update all three readings from the GraphixThree (Number 1)
+-- | if a gauge is not connected or transmitting data fails,
+-- | a Nothing is returned
+graphixThree1PressureUpdate :: Measurements -> IO (Maybe Double, Maybe Double, Maybe Double)
+graphixThree1PressureUpdate m = do
+  gt1 <- openSerial graphixThree1Port defaultSerialSettings { commSpeed = CS38400 }
+
+  -- request pressure from gauge 1
+  _ <- send gt1 $ fromJust graphixThreeRequestA
+  graphixThree1AnswerA <- recv gt1 255
+
+  threadDelay 100000
+
+-- request pressure from gauge 2
+  _ <- send gt1 $ fromJust graphixThreeRequestB
+  graphixThree1AnswerB <- recv gt1 255
+
+  threadDelay 100000
+
+  -- request pressure from gauge 3
+  _ <- send gt1 $ fromJust graphixThreeRequestC
+  graphixThree1AnswerC <- recv gt1 255
+
+  closeSerial gt1
+
+  let graphixThree1TempA = AB.parseOnly GT.parsePressure graphixThree1AnswerA
+      graphixThree1TempB = AB.parseOnly GT.parsePressure graphixThree1AnswerB
+      graphixThree1TempC = AB.parseOnly GT.parsePressure graphixThree1AnswerC
+
+  if isLeft graphixThree1TempA
+    then
+      if isLeft graphixThree1TempB
+        then
+          if isLeft graphixThree1TempC
+            then return (Nothing, Nothing, Nothing)
+            else return (Nothing, Nothing, Just $ fromRight graphixThree1TempA)
+        else
+          if isLeft graphixThree1TempC
+            then return (Nothing, Just $ fromRight graphixThree1TempB, Nothing)
+            else return (Nothing, Just $ fromRight graphixThree1TempB, Just $ fromRight graphixThree1TempC)
+    else
+      if isLeft graphixThree1TempB
+        then
+          if isLeft graphixThree1TempC
+            then return (Just $ fromRight graphixThree1TempA, Nothing, Nothing)
+            else return (Just $ fromRight graphixThree1TempA, Nothing, Just $ fromRight graphixThree1TempC)
+        else
+          if isLeft graphixThree1TempC
+            then return (Just $ fromRight graphixThree1TempA, Just $ fromRight graphixThree1TempB, Nothing)
+            else return (Just $ fromRight graphixThree1TempA, Just $ fromRight graphixThree1TempB, Just $ fromRight graphixThree1TempC)
+  where
+    graphixThree1Port = m ^. graphixThree1 . gt1Port
+    graphixThreeRequestA = GT.createCommandGTString (GT.AskPressure GT.A)
+    graphixThreeRequestB = GT.createCommandGTString (GT.AskPressure GT.B)
+    graphixThreeRequestC = GT.createCommandGTString (GT.AskPressure GT.C)
+
+toggleGraphixThree1 :: Measurements -> Measurements
+toggleGraphixThree1 m = m & graphixThree1 . gt1Enabled .~ not currVal
+  where
+    currVal = m ^. graphixThree1 . gt1Enabled
+
+{- ============== -}
+{- GraphixThree 2 -}
+{- ============== -}
+-- | update all three readings from the GraphixThree (Number 1)
+-- | if a gauge is not connected or transmitting data fails,
+-- | a Nothing is returned
+graphixThree2PressureUpdate :: Measurements -> IO (Maybe Double, Maybe Double, Maybe Double)
+graphixThree2PressureUpdate m = do
+  gt2 <- openSerial graphixThree2Port defaultSerialSettings { commSpeed = CS38400 }
+  _ <- send gt2 $ fromJust graphixThreeRequestA
+  graphixThree2AnswerA <- recv gt2 255
+
+  threadDelay 100000
+
+  _ <- send gt2 $ fromJust graphixThreeRequestB
+  graphixThree2AnswerB <- recv gt2 255
+
+  threadDelay 100000
+
+  _ <- send gt2 $ fromJust graphixThreeRequestC
+  graphixThree2AnswerC <- recv gt2 255
+
+  closeSerial gt2
+
+  let graphixThree2TempA = AB.parseOnly GT.parsePressure graphixThree2AnswerA
+      graphixThree2TempB = AB.parseOnly GT.parsePressure graphixThree2AnswerB
+      graphixThree2TempC = AB.parseOnly GT.parsePressure graphixThree2AnswerC
+
+  if isLeft graphixThree2TempA
+    then
+      if isLeft graphixThree2TempB
+        then
+          if isLeft graphixThree2TempC
+            then return (Nothing, Nothing, Nothing)
+            else return (Nothing, Nothing, Just $ fromRight graphixThree2TempA)
+        else
+          if isLeft graphixThree2TempC
+            then return (Nothing, Just $ fromRight graphixThree2TempB, Nothing)
+            else return (Nothing, Just $ fromRight graphixThree2TempB, Just $ fromRight graphixThree2TempC)
+    else
+      if isLeft graphixThree2TempB
+        then
+          if isLeft graphixThree2TempC
+            then return (Just $ fromRight graphixThree2TempA, Nothing, Nothing)
+            else return (Just $ fromRight graphixThree2TempA, Nothing, Just $ fromRight graphixThree2TempC)
+        else
+          if isLeft graphixThree2TempC
+            then return (Just $ fromRight graphixThree2TempA, Just $ fromRight graphixThree2TempB, Nothing)
+            else return (Just $ fromRight graphixThree2TempA, Just $ fromRight graphixThree2TempB, Just $ fromRight graphixThree2TempC)
+  where
+    graphixThree2Port = m ^. graphixThree2 . gt2Port
+    graphixThreeRequestA = GT.createCommandGTString (GT.AskPressure GT.A)
+    graphixThreeRequestB = GT.createCommandGTString (GT.AskPressure GT.B)
+    graphixThreeRequestC = GT.createCommandGTString (GT.AskPressure GT.C)
+
+toggleGraphixThree2 :: Measurements -> Measurements
+toggleGraphixThree2 m = m & graphixThree2 . gt2Enabled .~ not currVal
+  where
+    currVal = m ^. graphixThree2 . gt2Enabled
 
 
 {- ########################################################################## -}
@@ -409,27 +677,47 @@ lakeShoreParser = do
 {- ########################################################################## -}
 initColdIon :: ColdIon
 initColdIon = ColdIon
-  { _ciEnabled = False
-  , _ciTask = CI.GetDevName
-  , _ciPort = "/dev/ttyUSB0"
-  , _ciChannel = 1
+  { _ciEnabled  = False
+  , _ciTask     = CI.GetDevName
+  , _ciPort     = "/dev/ttyUSB0"
+  , _ciChannel  = 1
   , _ciPressure = Nothing
-  , _ciDevName = Nothing
+  , _ciDevName  = Nothing
   }
 
 initLakeShore :: LakeShore
 initLakeShore = LakeShore
-  { _lsEnabled = False
-  , _lsTask = LS.AskTemperature LS.A
-  , _lsPort = "/dev/ttyUSB1"
+  { _lsEnabled      = False
+  , _lsTask         = LS.AskTemperature LS.A
+  , _lsPort         = "/dev/ttyUSB1"
   , _lsTemperatures = (Nothing, Nothing)
-  , _lsDevName = Nothing
+  , _lsDevName      = Nothing
+  }
+
+initGraphixThree1 :: GraphixThree1
+initGraphixThree1 = GraphixThree1
+  { _gt1Enabled   = False
+  , _gt1Task      = GT.AskPressure GT.A
+  , _gt1Port      = "/dev/ttyUSB2"
+  , _gt1Pressures = (Nothing, Nothing, Nothing)
+  , _gt1DevName   = Nothing
+  }
+
+initGraphixThree2 :: GraphixThree2
+initGraphixThree2 = GraphixThree2
+  { _gt2Enabled   = False
+  , _gt2Task      = GT.AskPressure GT.A
+  , _gt2Port      = "/dev/ttyUSB3"
+  , _gt2Pressures = (Nothing, Nothing, Nothing)
+  , _gt2DevName   = Nothing
   }
 
 initMeasurement :: Measurements
 initMeasurement = Measurements
- { _coldIon = initColdIon
+ { _coldIon   = initColdIon
  , _lakeShore = initLakeShore
+ , _graphixThree1 = initGraphixThree1
+ , _graphixThree2 = initGraphixThree2
  }
 
 
@@ -438,9 +726,9 @@ initMeasurement = Measurements
 {- ########################################################################## -}
 main :: IO ()
 main = do
-  chan <- newBChan 10
-  forkIO $ forever $ do
-    writeBChan chan Tick
-    threadDelay 2500000
+  chan <- newBChan 50
+  _ <- forkIO $ forever $ do
+         writeBChan chan Tick
+         threadDelay 2500000
 
   void $ customMain (V.mkVty V.defaultConfig) (Just chan) app initMeasurement
